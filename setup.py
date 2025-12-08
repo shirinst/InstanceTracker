ebac09297b704c7f62f0fcbba18a1e160f7a1c4e
ShirinST <97607383+shirinst@users.noreply.github.com>
2025-12-08 00:23:26 +0000

    new file:   IT/bin/Activate.ps1 	new file:   IT/bin/activate 	new file:   IT/bin/activate.csh 	new file:   IT/bin/activate.fish 	new file:   IT/bin/pip 	new file:   IT/bin/pip3 	new file:   IT/bin/pip3.12 	new file:   IT/bin/python 	new file:   IT/bin/python3 	new file:   IT/bin/python3.12 	new file:   IT/lib64 	new file:   IT/pyvenv.cfg 	modified:   setup.py 	modified:   src/instance_tracker/__init__.py

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name="InstanceTracker",
-    version="0.1.0",
+    version="0.1.1",
     description="A metaclass for tracking object instances with weakref and context managers",
     author="ShirinST",
     author_email="e.v.shirinst@outlook.com",
     url="https://github.com/shirinst/InstanceTracker",
-    packages=find_packages("src"),
+    packages=find_packages("src/instance_tracker"),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.10',
 )
--- src/instance_tracker/__init__.py
+++ src/instance_tracker/__init__.py
@@ -1,4 +1,5 @@
 """
 InstanceTracker - A metaclass for tracking object instances with weakref and context managers
 """
-from instance_tracker import instance_tracker
+from instance_tracker import InstanceCounter
+print(InstanceCounter.get_global_stats())
